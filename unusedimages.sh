31fc08def5139db860687965bc724d426ce451d1
zhoujie <zhoujie_903@163.com>
2016-10-15 10:16:14 +0800

    docs(unusedimages.sh): 增加注释

--- unusedimages.sh
+++ unusedimages.sh
@@ -1,8 +1,44 @@
+#!/bin/bash
+##author: zhoujie<13456774460@139.com>
+##查找xcdoe iOS APP项目xcassets中不再使用的图片
+
+#ls
+#-R      Recursively list subdirectories encountered
+#-l      List in long format.
+
+#ls -R -l | grep "^d"
+#过滤出目录文件
+
+#grep "\.imageset"
+#在目录文件中过滤出.imageset目录
+#结果：drwxr-xr-x@ 6 zhoujie  staff  204 Jul 11 08:50 personal_cell_icon_ticket.imageset
+
+#grep -o ":\d\d .\+\.imageset"
+#过度截取出图片文件名
+#结果：:50 personal_cell_icon_ticket.imageset
+
+#sed -n -e "s/:[0-9][0-9] //;s/\.imageset$//gp"
+#截取出图片文件名
+#结果：personal_cell_icon_ticket
+
 images=$( ls -R -l | grep "^d" | grep "\.imageset" | grep -o ":\d\d .\+\.imageset" | sed -n -e "s/:[0-9][0-9] //;s/\.imageset$//gp" )
-> unusedImages.txt
+
+##清空文件
+unusedImages=unusedImages.txt
+> $unusedImages
+
+#ag
+#A code-searching tool similar to ack, but faster. http://geoff.greer.fm/ag/
+#https://github.com/ggreer/the_silver_searcher
+
+#--case-sensitive     Match case sensitively
+#---ignore PATTERN     Ignore files/directories matching PATTERN
+#                          (literal file/directory names also allowed)
+#--ignore-dir NAME    Alias for --ignore for compatibility with ack.
+
 for i in $images; do
-	ag --case-sensitive --ignore allimages.txt --ignore unusedImages.txt --ignore-dir "Images.xcassets" "$i" './'
+	ag --case-sensitive --ignore $unusedImages --ignore-dir "Images.xcassets" "$i" './'
 	if [[ $? -ne 0 ]]; then
-		echo "$i" >> unusedImages.txt
+		echo "$i" >> $unusedImages
 	fi
 done
