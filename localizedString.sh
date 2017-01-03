5f886742c6d355f6b18efb7f78efd04bdb554adc
zhoujie <zhoujie_903@163.com>
2017-01-03 23:30:04 +0800

    feat: 增加字符串国际化替换

--- localizedString.sh
+++ localizedString.sh
@@ -7,36 +7,75 @@
 #set -x
 
 #必须先声明
-declare -A dic
+#declare -A dic
 declare -a ss
 declare -a kk 
 
-#grep -o -E '=\s*".+"' $1
+#提取values
+##############################################################
+sed -n -e 's/\(.*=\s*\)\(".*"\);/\2/p' $1 >values.txt
+printf "\n" >>values.txt
+count_values=$(cat ./values.txt|wc -l)
+printf "values:%s\n" ${count_values}
+##############################################################
 
-strings=$( sed -n -e 's/\(.*=\s*\)\(".*"\);/\2/p'  $1)
-for item in $strings; do
-	#echo ${item}
-	ss[${#ss[@]}]=${item}
+
+#提取keys
+##############################################################
+sed -n -e 's/\(".*"\)\(\s*=\s*.*;\)/\1/p'  $1 >keys.txt
+printf "\n" >>keys.txt
+count_keys=$(cat ./keys.txt|wc -l)
+printf "keys:%s\n" ${count_keys}
+##############################################################
+
+
+if [[ $count_values != $count_keys ]]; then
+	echo "键与值数量不相等"
+	exit -1
+fi
+
+
+##############################################################
+n=1  
+while ((n<=${count_values}))  
+do  
+    ss[$n]=$(cat values.txt | sed -n "${n}p" ) 
+    #printf "|%s|\n" "${ss[$n]}"
+    ((n+=1))      
 done
+##############################################################
 
-keys=$( sed -n -e 's/\(".*"\)\(\s*=\s*.*;\)/\1/p'  $1)
-for item in $keys; do
-	#echo ${item}
-	kk[${#kk[@]}]=${item}
+
+##############################################################
+n=1  
+while ((n<=${count_keys}))  
+do  
+    kk[$n]=$(cat keys.txt | sed -n "${n}p" ) 
+    #printf "|%s|\n" "${kk[$n]}"
+    ((n+=1))      
 done
+##############################################################
 
+
+#建立字典：
+##############################################################
+# for (( i = 0; i < ${#ss[@]}; i++ )); do
+# 	#printf "$i:|%s|%s\n" "${kk[$i]}" "${ss[$i]}"
+# 	#dic["${kk[$i]}"]=${ss[${i}]}
+# done
+##############################################################
+
+
+#字符串替换
+##############################################################
 for (( i = 0; i < ${#ss[@]}; i++ )); do
-	dic[${kk[${i}]}]=${ss[${i}]}
-	#echo kk ${kk[${i}]}
-	#echo ss ${ss[${i}]}  
+	#printf "正在替换:|%s|\n" "${kk[$i]}"
+	if [[ "${kk[$i]}" == '""' || -z "${kk[$i]}" ]]; then
+		echo 1 >/dev/null	
+	else
+		printf "正在替换:|%s| --> |%s|\n" "${ss[${i}]}" "${kk[${i}]}"
+		sed -i -e "s/@${ss[${i}]}/NSLocalizedString(@${kk[${i}]}, nil)/" *.m
+	fi
 done
-
-#echo ${#ss[@]}
-#echo ${#kk[@]}
-#echo ${dic[*]}
-
-for (( i = 0; i < ${#ss[@]}; i++ )); do
-	#echo ${ss[${i}]} 
-	sed -i -e "s/@${ss[${i}]}/NSLocalizedString(@${kk[${i}]}, nil)/" *.m
-done
+##############################################################
 
