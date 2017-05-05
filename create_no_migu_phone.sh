ca22731594ac1c6c6dad343c7fc3c8aaee51d745
zhoujie <zhoujie_903@163.com>
2017-05-05 15:52:49 +0800

    refactor: 修正shellcheck给出的警告

--- create_no_migu_phone.sh
+++ create_no_migu_phone.sh
@@ -10,9 +10,10 @@ readonly miguSortedFile=sorted_migu.txt
 
 function rand() {  
     local min=$1  
-    local max=$(($2-$min+1))  
-    local num=$(cat /dev/urandom | head -n 11 | cksum | awk -F ' ' '{print $1}') 
-    echo $(($num%$max+$min)) 
+    local max=$(($2-min+1))  
+    local num
+    num=$(head -n 11 < /dev/urandom | cksum | awk -F ' ' '{print $1}') 
+    echo $((num%max+min)) 
 }
 
 
@@ -20,7 +21,8 @@ function create_migu_phones () {
 	echo "生成咪咕通信录:${miguSortedFile}......."
 
 	##找出所有子公司目录下的sorted_all.txt文件	
-	local company=$(find . -mindepth 1 -name sorted_all.txt);
+	local company
+	company=$(find . -mindepth 1 -name sorted_all.txt)
 
 	##读取所有sorted_all.txt文件内容，并排序和剔除重复号码，最后写入${miguSortedFile}文件
 	cat $company | sort -u > ${miguSortedFile} 
@@ -30,14 +32,14 @@ function create_migu_phones () {
 function create_random_phones () {
 	echo "生成随机号码:${randSortedFile}......"
 
-	local readonly randFile=rand.txt
+	local randFile=rand.txt
 
-	> ${randFile}
+	true > ${randFile}
 
 	##生成randFile(rand.txt)文件
-	for (( i = 0; i <= 3000; i = $((i + 1)) )); do
+	for (( i = 0; i < 3000; i = $((i + 1)) )); do
 		rnd=$(rand 13400000000 13999999999)
-		printf "%d|%d\n" $rnd $rnd >>${randFile}
+		printf "%d|%d\n" "$rnd" "$rnd" >>${randFile}
 	done
 
 	#排序随机号码
@@ -56,7 +58,7 @@ create_random_phones
 ##排除已在前面月份中发过的号码
 function comm_months_and_migu_phones () {
 
-	local readonly noMiguFile=no_migu_phone.txt
+	local noMiguFile=no_migu_phone.txt
 
 	echo "生成排除咪咕的通信录:${noMiguFile}......."
 
@@ -90,7 +92,4 @@ rm ${randSortedFile}
 find . -name "*.json" -exec rm {} \;
 
 echo Done!!!
-
-
-
  
