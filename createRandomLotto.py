110af430d1050304cc1b9b4f7a3e50492bf95eec
zhoujie <zhoujie_903@163.com>
2018-03-20 14:08:01 +0800

    refactor: 抽取格式化输出函数

--- createRandomLotto.py
+++ createRandomLotto.py
@@ -3,24 +3,33 @@
 import random
 
 
+def output(item):
+    for n in item["firt"]:
+        print("%02d " % (n), end="")
+    print(" - ", end="")
+    for n in item["last"]:
+        print("%02d " % (n), end="")
+    print("\n")
+
+
 def createItem():
     """创建一注彩票"""
     lottery_36 = range(1, 36)
-    front_5 = random.sample(lottery_36, 5)
-    front_5 = sorted(front_5)
+    lottery_first = random.sample(lottery_36, 5)
+    lottery_first.sort()
 
     lottery_12 = range(1, 13)
-    end_2 = random.sample(lottery_12, 2)
-    end_2 = sorted(end_2)
-    print("%02d %02d %02d %02d %02d - %02d %02d" %
-          (front_5[0], front_5[1], front_5[2], front_5[3], front_5[4], end_2[0], end_2[1]))
+    lottery_last = random.sample(lottery_12, 2)
+    lottery_last.sort()
 
+    return {"firt": lottery_first, "last": lottery_last}
 
 
 def createLotto(count=5):
     """创建count注彩票"""
     for i in range(count):
-        createItem()
+        item = createItem()
+        output(item)
 
 
 createLotto()
